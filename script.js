018297da3990a1dad22f7e3e4425982e9aa77868
Fabricio Veloso <fabraccho@gmail.com>
2023-11-08 16:25:48 -0300

    Comit com especificações PWA

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
-// Durante a primeira interação
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the browser's default prompt
+  event.preventDefault();
+
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
+
+  // Show your custom install UI or button
+  showInstallButton();
+});
+
+function showInstallButton() {
+  // Display a button or UI element to trigger the install
+  const installButton = document.getElementById('install-button');
+
+  // Show the button
+  installButton.style.display = 'block';
+
+  // Add a click event listener to trigger the installation
+  installButton.addEventListener('click', () => {
+    // Hide the button
+    installButton.style.display = 'none';
+
+    // Show the browser's install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+
+      // Clear the deferredPrompt variable, as it can only be used once
+      deferredPrompt = null;
+    });
+  });
+}
+
 function registrarPreferenciaTelaCheia() {
-    localStorage.setItem('preferenciaTelaCheia', 'true');
-    criarAtalhoNaTelaInicial();
-  }
-  
-  // Nas visitas subsequentes
-  function verificarEAplicarTelaCheia() {
-    const preferenciaTelaCheia = localStorage.getItem('preferenciaTelaCheia');
-    if (preferenciaTelaCheia === 'true') {
-      // Chame o método para colocar o aplicativo em tela cheia
-      requestFullScreen();
-    }
-  }
-  
-  // Função para criar o atalho na tela inicial
-  function criarAtalhoNaTelaInicial() {
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  
-    if (isIOS) {
-      alert('Para adicionar o aplicativo à tela inicial, toque no ícone de compartilhamento e escolha "Adicionar à Tela Inicial".');
-    } else {
-      alert('Para adicionar o aplicativo à tela inicial, toque no botão do navegador e escolha "Adicionar à Tela Inicial".');
-    }
-  }
-  
-  // Função para solicitar tela cheia
-  function requestFullScreen() {
-    const element = document.documentElement;
-  
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    }
-  }
-  
-  // Exemplo de chamada na inicialização do aplicativo
+  localStorage.setItem('preferenciaTelaCheia', 'true');
   verificarEAplicarTelaCheia();
-  
+}
+
+function verificarEAplicarTelaCheia() {
+  const preferenciaTelaCheia = localStorage.getItem('preferenciaTelaCheia');
+  if (preferenciaTelaCheia === 'true') {
+    // Chame o método para colocar o aplicativo em tela cheia
+    requestFullScreen();
+  }
+}
+
+function requestFullScreen() {
+  const element = document.documentElement;
+
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  }
+}
